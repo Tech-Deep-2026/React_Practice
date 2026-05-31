2838ba19835422a1afca0fa15bc6617980d49b19
Dipali Shrawane <tech.dipshrawane@gmail.com>
2026-05-31 12:31:56 +0530

    React Element

--- App.js
+++ App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement('div', { id: 'parent' },[
-    React.createElement('div', { id: 'child' },[
-         React.createElement('h1', {}, 'I am a h1 tag'),
-         React.createElement('h2', {}, 'I am a h2 tag'),
-    ]),
-    React.createElement('div', { id: 'child2' },[
-    React.createElement('h1', {}, 'I am an h1 tag'),
-    React.createElement('h2', {}, 'I am an h2 tag'),
-]),
-]);
-console.log(parent);
+const jsxHeading = (
+    <h1 className='heading' tabIndex="5">Namaste React</h1>
+);
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(jsxHeading);
 
